80d6142d06f21a375cc6393736258822c3806696
varga madalina <madalina.g.varga@gmail.com>
2023-05-08 20:13:29 +0300

    redirect retry

--- Platforma-educationalaUI/src/config.ts
+++ Platforma-educationalaUI/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  baseApiUrl: process.env.NODE_ENV == "development" ? "/api" : "https://platformaeducationala.azurewebsites.net",
+  baseApiUrl: "/api",
 };
 
 const currencyFormatter = Intl.NumberFormat("en-US", {
